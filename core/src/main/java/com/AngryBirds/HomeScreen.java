c9d3cb43cca7e845b6d8e382d7fd6ba0f2e5c2ae
Aryamaan Singh <aryamaan.singh05@gmail.com>
2024-10-20 13:36:38 +0530

    Added Game Directory

---
+++ core/src/main/java/com/AngryBirds/HomeScreen.java
@@ -0,0 +1,126 @@
+package com.AngryBirds;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class HomeScreen implements Screen {
+    private Stage stage;
+    private Skin UIskin;
+    private Texture background;
+    private SpriteBatch spriteBatch;
+
+    private Sprite PLAY;
+    private Sprite LOGO;
+    private Sprite SETTINGS;
+
+    @Override
+    public void show() {
+        spriteBatch = new SpriteBatch();
+        background = new Texture(Gdx.files.internal("homeBackground.png"));
+
+        Texture playTexture = new Texture(Gdx.files.internal("playButton.png"));
+        PLAY = new Sprite(playTexture);
+        PLAY.setSize(150f, 50f);
+
+        Texture logoTexture = new Texture(Gdx.files.internal("angryBirdsLogo.png"));
+        LOGO = new Sprite(logoTexture);
+        LOGO.setSize(200f, 20f);
+
+        Texture settingsTexture = new Texture(Gdx.files.internal("settingsButton.png"));
+        SETTINGS = new Sprite(settingsTexture);
+        SETTINGS.setSize(200f, 20f);
+
+        UIskin = new Skin(Gdx.files.internal("skins/uiskin.json"));
+
+        stage = new Stage(new ScreenViewport());
+        Gdx.input.setInputProcessor(stage);
+
+        Table table = new Table();
+        table.setFillParent(true);
+
+        Image logoButtonImage = new Image(LOGO);
+        logoButtonImage.setSize(150f, 10f); // Set size for the play button image
+
+        table.add(logoButtonImage).size(700f, 100f).center().padBottom(20);
+        table.row();
+
+        Image playButtonImage = new Image(PLAY);
+        playButtonImage.setSize(150f, 50f);
+
+        table.add(playButtonImage).size(300f, 50f).center().padBottom(20);
+        table.row();
+
+        Image settingsButtonImage = new Image(SETTINGS);
+        settingsButtonImage.setSize(150f, 50f);
+
+        table.add(settingsButtonImage).size(150f,50f).center();
+        stage.addActor(table);
+
+        ClickListener playButtonListener = new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+            }
+        };
+
+        ClickListener settingsButtonListener = new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+            }
+        };
+
+        playButtonImage.addListener(playButtonListener);
+        settingsButtonImage.addListener(settingsButtonListener);
+    }
+
+    @Override
+    public void render(float v) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+
+        OrthographicCamera camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.update();
+
+        spriteBatch.setProjectionMatrix(camera.combined);
+        spriteBatch.begin();
+        spriteBatch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        spriteBatch.end();
+
+       // stage.act(Math.min(Gdx.graphics.getDeltaTime(), 1 / 30f));
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void pause() {}
+
+    @Override
+    public void resume() {}
+
+    @Override
+    public void hide() {
+        dispose();
+    }
+
+    @Override
+    public void dispose() {
+        UIskin.dispose();
+        stage.dispose();
+        spriteBatch.dispose();
+        LOGO.getTexture().dispose();
+        PLAY.getTexture().dispose();
+        background.dispose();
+    }
+}
---
+++ core/src/main/java/com/AngryBirds/SettingsScreen.java
@@ -0,0 +1,124 @@
+package com.AngryBirds;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.EventListener;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+
+public class SettingsScreen implements Screen {
+    private Stage stage;
+    private Skin UIskin;
+    private SpriteBatch spriteBatch;
+
+    private Sprite LOGO;
+    private Label VOLUME;
+    private Label BRIGHTNESS;
+    private Sprite BACK;
+
+    @Override
+    public void show() {
+        spriteBatch=new SpriteBatch();
+
+        Texture logoTexture = new Texture(Gdx.files.internal("settingsLogo.png"));
+        LOGO = new Sprite(logoTexture);
+        LOGO.setSize(200f, 20f);
+
+        Texture backTexture = new Texture(Gdx.files.internal("back.png"));
+        LOGO = new Sprite(backTexture);
+        LOGO.setSize(200f, 20f);
+
+
+        Table table=new Table();
+        table.setFillParent(true);
+
+        Image logoButtonImage=new Image(LOGO);
+        logoButtonImage.setSize(150f, 10f);
+
+        table.add(logoButtonImage).size(700f,10f).center().padBottom(20);
+        table.row();
+
+        Label VOLUME = new Label("Game Volume", UIskin);
+        Slider vslide = new Slider(0, 100, 1, false, UIskin);
+        vslide.setValue(50);
+
+        table.add(VOLUME).center().padBottom(10);
+        table.row();
+        table.add(vslide).width(300).center().padBottom(30);
+        table.row();
+
+        Label BRIGHTNESS = new Label("Volume", UIskin);
+        Slider bslide = new Slider(0, 100, 1, false, UIskin);
+        bslide.setValue(50);
+
+        table.add(BRIGHTNESS).center().padBottom(10);
+        table.row();
+        table.add(bslide).width(300).center().padBottom(30);
+        table.row();
+
+        stage.addActor(table);
+
+        ChangeListener volumeListner=new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+            }
+        };
+
+        ChangeListener brightnessListner=new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+            }
+        };
+
+        vslide.addListener((EventListener) volumeListner);
+        bslide.addListener((EventListener) brightnessListner);
+
+    }
+
+    @Override
+    public void render(float v) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+
+        OrthographicCamera camera = new OrthographicCamera();
+        camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+        camera.update();
+
+        spriteBatch.setProjectionMatrix(camera.combined);
+        spriteBatch.begin();
+        spriteBatch.end();
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+        dispose();
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
