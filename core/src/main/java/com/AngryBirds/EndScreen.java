5a45f6315903ba73503e0a70d99f2bc8f5a78622
Aryamaan Singh <aryamaan.singh05@gmail.com>
2024-10-25 16:01:51 +0530

    update

---
+++ core/src/main/java/com/AngryBirds/EndScreen.java
@@ -0,0 +1,99 @@
+package com.AngryBirds;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+
+public class EndScreen implements Screen {
+    private Stage stage;
+    private Skin UIskin;
+    private SpriteBatch spriteBatch;
+    private Game game;
+
+    public EndScreen(Game game) {
+        this.game = game;
+    }
+
+    @Override
+    public void show() {
+        spriteBatch = new SpriteBatch();
+
+        UIskin = new Skin(Gdx.files.internal("skins/uiskin.json"));
+
+        Texture headerTexture = new Texture(Gdx.files.internal("levelComplete.png"));
+        Image headerImage = new Image(headerTexture);
+
+        Texture scoreTexture = new Texture(Gdx.files.internal("score.png"));
+        Image scoreImage = new Image(scoreTexture);
+
+        TextButton nextLevelButton = new TextButton("Next Level", UIskin);
+
+
+        scoreImage.setWidth(200);
+        scoreImage.setHeight(100);
+
+        stage = new Stage(new ScreenViewport());
+
+        Table table = new Table();
+        table.setFillParent(true);
+        table.center();
+
+        table.add(headerImage).padBottom(20);
+        table.row();
+        table.add(scoreImage).padBottom(20);
+        table.row();
+        table.add(nextLevelButton).padBottom(20);
+
+        stage.addActor(table);
+        Gdx.input.setInputProcessor(stage);
+
+        nextLevelButton.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+            }
+        });
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0, 0, 0, 1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        stage.act(delta);
+        stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
+    }
+
+    @Override
+    public void pause() {}
+
+    @Override
+    public void resume() {}
+
+    @Override
+    public void hide() {
+        dispose();
+    }
+
+    @Override
+    public void dispose() {
+        stage.dispose();
+        UIskin.dispose();
+        spriteBatch.dispose();
+    }
+}
