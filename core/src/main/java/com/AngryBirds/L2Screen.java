fd8986b30cf3bdc91be0a055f85dfae2fd538278
Aryamaan Singh <aryamaan23139@iiitd.ac.in>
2024-11-26 22:07:36 +0530

    .

--- core/src/main/java/com/AngryBirds/L2Screen.java
+++ core/src/main/java/com/AngryBirds/L2Screen.java
@@ -19,10 +19,9 @@ import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
 import java.io.*;
-import java.util.HashMap;
-import java.util.LinkedList;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
+
+import static java.lang.Math.max;
 
 public class L2Screen implements Screen {
     private static final float PPM = 100f;
@@ -39,6 +38,7 @@ public class L2Screen implements Screen {
     private Texture woodBoxtex;
     private Texture pigTexture;
     private Texture king_pigTexture;
+    private int highscore;
 
     private Texture redBirdTexture;
     private Texture yellowBirdTexture;
@@ -465,7 +465,36 @@ public class L2Screen implements Screen {
         }
 
         if (pigs.isEmpty() && totalTime<=20) {
-//            game.setScreen(new LevelSuccessScreen(this.game));
+            float score = (20 - totalTime) * 100;
+            highscore = max(highscore, (int)score);
+
+            List<String[]> data = new ArrayList<>();
+
+            try (BufferedReader br = new BufferedReader(new FileReader("highscore.csv"))) {
+                String line;
+
+                while ((line = br.readLine()) != null) {
+                    data.add(line.split(","));
+                }
+
+                if (data.size() > 1) {
+                    highscore = Integer.parseInt(data.get(1)[2]);
+                    data.get(1)[2] = String.valueOf(max(highscore, (int)score));
+                }
+
+                // Write the updated data back to the file
+                try (BufferedWriter bw = new BufferedWriter(new FileWriter("highscore.csv"))) {
+                    for (String[] row : data) {
+                        bw.write(String.join(",", row));
+                        bw.newLine();
+                    }
+                }
+            }
+            catch (IOException e) {
+                System.err.println("Error updating the file: " + e.getMessage());
+            }
+
+            game.setScreen(new LevelSuccessScreen(this.game, score, 2));
         }
 
         if(totalTime>20){
